4bbb60ed9e65ac5bcce8db0c937464281b4f7d51
Cate James <catejames2005@icloud.com>
2026-08-26 18:38:13 +0100

    New lines challenge

--- LearningC/myfirstprogram.c
+++ LearningC/myfirstprogram.c
@@ -1,10 +1,11 @@
 // cd LearningC
-// gcc myfirstprogram.c -o myfirstprogram && ./myfirstprogram   
+// gcc myfirstprogram.c -o myfirstprogram && ./myfirstprogram
 
 #include <stdio.h> 
 
 int main() {
-    printf("Hello World!");
-    printf("Have a great day!");
+    printf("Hello World!\n\n");
+    printf("I am learning C.\n");
+    printf("And it is awesome!");
     return 0;
 }
