05b13f01b8be1fcc9448584967b8f6d986ff6a07
Cate James <catejames2005@icloud.com>
2026-08-27 10:58:20 +0100

    Variables in C

---
+++ LearningC/Information/VARIABLES.md
@@ -0,0 +1,9 @@
+# Variables 
+
+Variables are containers for storing data values, like numbers and characters.
+
+In C, variables must have a specific type, which tells the program what kind of data the variable can store.
+
+- int - stores whole numbers (integers), such as 123 or -123
+- float - stores numbers with decimals, such as 19.99 or -19.99
+- char - stores a single character, such as 'a' or 'B'. Characters are surrounded by single quotes
--- LearningC/myfirstprogram.c
+++ LearningC/myfirstprogram.c
@@ -6,6 +6,25 @@
 int main() {
     printf("Hello World!\n\n");
     printf("I am learning C.\n");
-    printf("And it is awesome!");
+    printf("And it is awesome!\n\n");
+
+    // Create variables
+    int myNum = 15; // Integer (whole number)
+    float myFloatNum = 5.99; // Floating point
+    char myLetter = 'D'; // Character
+
+    // Print variables
+    printf("Different variables in C:\n");
+    printf("%d\n", myNum);
+    printf("%f\n", myFloatNum);
+    printf("%c\n\n", myLetter);
+
+    printf("My favourite number is %d\n", myNum);
+
+    printf("My number is %d and my letter is %c\n", myNum, myLetter);
+
+    printf("My favourite number is %d\n", 15);
+    printf("My favourite letter is %c\n", 'D');
     return 0;
+
 }
